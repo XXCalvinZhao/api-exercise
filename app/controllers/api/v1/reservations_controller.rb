f27b2da76124fbbd978f98b471c024c1bf19d0ae
Calvin <xcalvin.zhao@gmail.com>
2017-08-28 22:59:53 +0800

    7-7 修改reservations API

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -4,18 +4,6 @@ before_action :authenticate_user!, :only => [:index] # 这会检查 index +这�
 
 def index
   @reservations = current_user.reservations
-
-  render :json => {
-    :data => @reservations.map { |reservation|
-      {
-        :booking_code => reservation.booking_code,
-        :train_number => reservation.train.number,
-        :seat_number => reservation.seat_number,
-        :customer_name => reservation.customer_name,
-        :customer_phone => reservation.customer_phone
-      }
-    }
-  }
 end
 
 def create
@@ -37,14 +25,6 @@ end
 
 def show
   @reservation = Reservation.find_by_booking_code!( params[:booking_code] )
-
-  render :json => {
-   :booking_code => @reservation.booking_code,
-   :train_number => @reservation.train.number,
-   :seat_number => @reservation.seat_number,
-   :customer_name => @reservation.customer_name,
-   :customer_phone => @reservation.customer_phone
-  }
 end
 
 def update
